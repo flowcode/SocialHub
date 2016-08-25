d7a4988839b8999a2460ee6ea03e6b932216cde0
Juan Manuel Aguero <juanma.aguero@gmail>
2016-08-25 14:36:19 -0300

    Add trans domain options

--- src/Flowcode/SocialHubBundle/Controller/WidgetController.php
+++ src/Flowcode/SocialHubBundle/Controller/WidgetController.php
@@ -9,17 +9,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class WidgetController extends Controller
 {
     /**
+     * Social options, about configured.
      * @Template()
      */
-    public function loginOptionsAction()
+    public function loginOptionsAction($transDomain = null)
     {
-
         $socialNetworkService = $this->get('socialhub.service.socialnetwork');
 
         $networks = $socialNetworkService->getLoginUrls();
 
         return array(
-            'networks' => $networks
+            'networks' => $networks,
+            'trans_domain' => $transDomain,
         );
     }
 }
